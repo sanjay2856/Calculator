c81100d669658f62f0b8124f9d5ccee9db870764
sanjay2856 <sanjay_259138@saitm.ac.in>
2025-09-18 11:49:44 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,13 @@
+    def calculator(a,b,operator):
+        if operator =="+":
+            print(a+b)
+        elif operator =="-":
+            print(a-b)
+        elif opertaor =="*":
+            print(a*b)
+        elif operator =="/":
+            print(a/b)
+
+    calculator(100,100,"+")
+        
+ 
